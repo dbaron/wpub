ec3961e2b7e5350cad6a8eb42fcadcf02d8f279c
Ivan Herman <ivan@ivan-herman.net>
2019-02-02 17:58:37 +0100

    Only cosmetic changes

--- common/js/webidl.js
+++ common/js/webidl.js
@@ -1353,28 +1353,19 @@ function pretty_print_webidl(webidl) {
 function full_webidl() {
     const add_idl = (idl) => {
         const target = document.getElementById('our-idl-index');
-        h2 = document.createElement('h2');
-        h2.textContent = 'IDL Index';
-        target.appendChild(h2);
-
         pre = document.createElement('pre');
         pre.textContent = idl;
-        pre.classList.add('idl','def');
+        pre.classList.add('idl','def','no-link-warnings');
         pre.id = "actual-idl-index";
         target.appendChild(pre);
     }
-
-
-    let full_idl_text = ''
-    document.querySelectorAll('pre.idl').forEach((node) => {
-        full_idl_text += node.textContent
-    });
-
-
+    
+    let full_idl_text = Array.from(document.querySelectorAll('pre.idl')).map((node) => node.textContent).join('\n');    
     add_idl(pretty_print_webidl(full_idl_text));
 }
 
 function clean_full_webidl() {
     const pre = document.getElementById('actual-idl-index');
     pre.querySelectorAll("*[id]").forEach(elem => elem.removeAttribute("id"));
+    // pre.classList.add('idl', 'def');
 }
